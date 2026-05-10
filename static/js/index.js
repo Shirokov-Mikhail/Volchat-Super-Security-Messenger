581f1ede1a1795417fb3c4aa8c941d1a625414c3
Misha <m9339323029@gmail.com>
2026-05-10 18:55:38 +0700

    add auth and registration pages

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,24 @@
 const register_button = document.getElementById("register-btn");
 const register_form = document.getElementById("register");
 const x_register = document.getElementById("close-register");
-register_button.addEventListener("click", (e) => {
+const auth_btn = document.getElementById("auth-btn");
+const auth_form = document.getElementById("autorisi");
+const x_close = document.getElementById("close_auth");
+
+
+register_button.addEventListener("click", () => {
     register_form.style.display = "flex";
 })
-x_register.addEventListener("click", (e) => {
+x_register.addEventListener("click", () => {
     register_form.style.display = "none";
+    auth_btn.style.display = "block";
+})
+auth_btn.addEventListener("click", () => {
+
+    auth_form.style.display = "flex";
+})
+x_close.addEventListener("click", () => {
+    auth_form.style.display = "none";
+
+
 })
